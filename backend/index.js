564e121dfbae9b6f166344bdc692f096ecf1d911
srijanid <srijanidas06@gmail.com>
2024-09-27 15:07:07 +0530

    db connected

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './db/connectDB.js';
-//import authRoutes from './routes/auth.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5500;
 
 app.get("/",(req, res)=>{
    res.send("Hello World123!"); 
