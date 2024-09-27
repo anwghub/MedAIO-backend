cdaeed679867b9566927ca8c7dbc3bc2e33aa8c2
Anwesa Ghosh <anwesa2021@gmail.com>
2024-09-27 13:26:35 +0530

    gitignore added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { connectDB } from './db/connectDB.js';
-//import authRoutes from './routes/auth.route.js';
+//mport { connectDB } from './backend/db/connectDB';
+import authRoutes from './backend/routes/auth.route.js';
 
 dotenv.config();
 
@@ -17,6 +17,6 @@ app.use(express.json());
 app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () =>{
-    connectDB();
+    //connectDB();
     console.log("Server is running on port: ", PORT);
 })
