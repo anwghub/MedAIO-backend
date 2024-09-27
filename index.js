9e7594dc0a31f12d710d331fc6a9bc8f52f05ee3
Anwesa Ghosh <anwesa2021@gmail.com>
2024-09-27 14:01:11 +0530

    mongo added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-//mport { connectDB } from './backend/db/connectDB';
+import {connectDB } from './backend/db/connectDB.js';
 import authRoutes from './backend/routes/auth.route.js';
 
 dotenv.config();
@@ -17,6 +17,6 @@ app.use(express.json());
 app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () =>{
-    //connectDB();
+    connectDB();
     console.log("Server is running on port: ", PORT);
 })
